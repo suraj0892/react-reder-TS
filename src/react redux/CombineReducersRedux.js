d64f364f354b9a342668d3f58f2de945dab38b3f
Suraj <surajprabakark@gmail.com>
2022-02-07 20:19:57 +0530

    combine reducers

--- src/react redux/CombineReducersRedux.js
+++ src/react redux/CombineReducersRedux.js
@@ -2,10 +2,16 @@ const redux =  require('redux')
 const createStore  = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
-
+const BUY_ICE = 'BUY_ICE'
 function buyCake () {
     return {
         type : BUY_CAKE,
+        info :  'First Redux action'
+    };
+}
+function buyIce () {
+    return {
+        type : BUY_ICE,
         info :  'First Redux action'
     };
 }
@@ -13,11 +19,15 @@ function buyCake () {
 
 // (prevState, action) =>  new state
 
-const initialState = {
+const initialCake = {
     numOfCakes :  10
 }
 
-const reducer = (state = initialState , action) => {
+const initialIce = {
+    numOfIce :  20
+}
+
+const cakeReducer = (state = initialCake , action) => {
     switch(action.type){
         case BUY_CAKE : return  {
             ...state, numOfCakes : state.numOfCakes - 1 
@@ -26,12 +36,29 @@ const reducer = (state = initialState , action) => {
     }
 }
 
-const store = createStore(reducer) 
+const iceReducer = (state = initialIce , action) => {
+    switch(action.type){
+        case BUY_ICE : return  {
+            ...state, numOfIce : state.numOfIce - 1 
+        }
+        default : return state
+    }
+}
+
+const rootReducer = redux.combineReducers({
+    cake : cakeReducer, 
+    ice : iceReducer
+})
+
+const store = createStore(rootReducer) 
 console.log('initial state', store.getState())
 const unsubcribe = store.subscribe(() => console.log('update state' , store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIce())
+store.dispatch(buyIce())
+
 unsubcribe()
 
 
